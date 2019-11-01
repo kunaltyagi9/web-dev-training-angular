1dfe7c9e1f7705ceb709004634abe2f238a5cffe
Tyagi <768970@cognizant.com>
2019-11-01 12:22:29 +0530

    added angular router to the application

---
+++ AngularApp/first/src/app/user-list/user-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Observable } from 'rxjs';
+import { IUser } from '../user';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+})
+
+export class UserListComponent implements OnInit {
+  
+  toggleShowHide : boolean = true;
+  showColumns : boolean = false;
+  users$ : Observable<IUser[]>;
+
+
+  constructor(private _userService : UserService) { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.toggleShowHide = false;
+    },3000);
+    this.showColumns = true;
+    this.toggleShowHide = true;
+    this.users$ = this.getData();
+  }
+
+  getData() : Observable<IUser[]>{
+    return this._userService.getUsers();
+  }
+  
+
+}
---
+++ AngularApp/first/src/app/user.ts
@@ -0,0 +1,19 @@
+export interface IUser{
+    id : number;
+    name : string;
+    username : string;
+    email : string;
+    address : string;
+    street : string;
+    suite : string;
+    city : string;
+    zipcode : number;
+    geo : string;
+    lat : number;
+    lng : number;
+    phone : number;
+    website : string;
+    company : string;
+    catchPhrase : string;
+    bs : string;
+}
